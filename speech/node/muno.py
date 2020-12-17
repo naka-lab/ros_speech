b4ad5841410a77fefe10b5d06014957177ee10c1
test <test@test.com>
2020-12-18 03:05:09 +0900

    サンプル修正

--- speech/node/muno.py
+++ speech/node/muno.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # coding: utf-8
 
 import rospy
@@ -7,6 +7,25 @@ import os
 import time
 import codecs
 from julius.msg import speech_recres
+from julius.srv import SetGrammar
+
+# 音声認識辞書
+gram = """
+[GRAMMAR]
+hello : ろぼっと、こんにちわ
+morning : ろぼっと、おはよう
+grasp : ろぼっと、 $noun_obj をとって
+noise : つ
+
+
+[NOUN]
+$noun_obj
+obj1 : ぬいぐるみ
+obj2 : おちゃ
+obj3 : ぺっとぼとる
+
+"""
+
 
 
 class muno():
@@ -16,16 +35,22 @@ class muno():
 
 		self.sentenceSubs = rospy.Subscriber('/julius/recogres/small_vocab',  speech_recres ,self.recieve )
 		self.synthesisPub = rospy.Publisher( "jtalk/sentence" , std_msgs.msg.String, queue_size=10 )
+		
+		rospy.wait_for_service('julius/set_grammar')
+		set_grammar = rospy.ServiceProxy('julius/set_grammar', SetGrammar)
+		set_grammar( gram, [] )
 
 
 		rospy.spin()
 
 	def recieve(self,msg):
-		print msg.sentences[0]
-		print msg.sentence_id
+		print( msg.sentences[0] )
+		print( msg.sentence_id )
 
 		if msg.sentence_id=="hello":
 			self.synthesisPub.publish( "こんにちは" )
+		if msg.sentence_id=="morning":
+			self.synthesisPub.publish( "おはよう" )
 		elif msg.sentence_id=="grasp":
 			self.synthesisPub.publish( msg.noun_str[0] + "を取ります。" )
 
